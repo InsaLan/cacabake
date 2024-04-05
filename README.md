7ec479b850a2ef186d266dd3a2acb51c1e47782a
pixup1 <pixup1@yahoo.com>
2024-04-05 17:47:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# cacabake
+
+Simple bash script to prebake videos to text and display them in a terminal.
+
+## Requirements
+
+An `mpv` build with `libcaca`.
+
+## Usage
+
+Run `./cacabake.sh video.mp4` to create video.baked file, then `./cacabake video.baked` to display it.
---
+++ cacabake.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+videosext=("mp4 mpv mkv mov avi")
+input=0 # input is wrong by default
+
+if ! [ -f "$1" ]; then
+    echo "File not found."
+    exit 1
+fi
+
+if [[ ${1##*.} = baked ]]; then
+    input=1 # baked file to show
+else
+    for ext in $videosext
+    do
+        if [[ ${1##*.} = $ext ]]; then
+            input=2 # video to bake
+        fi
+    done
+fi
+
+if [ $input -eq 0 ]; then
+    echo "Wrong file format. Use a video or baked file."
+elif [ $input -eq 1 ]; then # playback
+    echo "aaa"
+elif [ $input -eq 2 ]; then # bake
+    touch ${1%%.*}.baked
+    # Add fps
+    echo test
+    mpv $1 --vo=caca --no-config --no-terminal # > ${1%%.*}.baked
+fi
