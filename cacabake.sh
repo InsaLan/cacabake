46e07a78e78c62daf584245a37e2eb5702426311
Lux <lux@insalan.fr>
2024-04-05 17:52:43 +0000

    Getting framerate

--- cacabake.sh
+++ cacabake.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
+
+sleep_fraction() {
+  /usr/bin/perl -e "select(undef, undef, undef, $1)"
+}
+
 videosext=("mp4 mpv mkv mov avi")
 input=0 # input is wrong by default
 
@@ -21,9 +26,15 @@ fi
 if [ $input -eq 0 ]; then
     echo "Wrong file format. Use a video or baked file."
 elif [ $input -eq 1 ]; then # playback
-    echo "aaa"
+    framerate=$(sed -n '1p' $1)
+#    for i in ($1)
+#    do
+        cat "${p}"
+        sleep_fraction 0.01428
+    done
 elif [ $input -eq 2 ]; then # bake
     touch ${1%%.*}.baked
     ffprobe $1 2>&1| grep ",* fps" | cut -d "," -f 5 | cut -d " " -f 2 > ${1%%.*}.baked 
     mpv $1 --vo=caca --untimed --framedrop=no --no-config --no-terminal >> ${1%%.*}.baked
+    echo "Bake successful"
 fi
